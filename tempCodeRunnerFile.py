ab2b8f2857bb6b8d167cecbe31ec1184c7b3cefe
samanadam <147505016+samanadam@users.noreply.github.com>
2025-03-03 00:20:27 +0300

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+import keyboard
+import time
+while not keyboard.is_pressed("q"):
+    if keyboard.is_pressed("z"):
+        keyboard.press_and_release("p")
+        while keyboard.is_pressed('z'):  # Z tuşuna basılı tutmayı engellemek için bekle
+            pass
+        
+    if keyboard.is_pressed("x"):
+        keyboard.press_and_release("enter")
+        while keyboard.is_pressed('x'):  # X tuşuna basılı tutmayı engellemek için bekle
+            pass
