6d734b0880c2d70870ed8af4529b6e4370a697cc
samanadam <147505016+samanadam@users.noreply.github.com>
2025-03-03 00:24:27 +0300

    pandenter.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,10 +3,10 @@ import time
 while not keyboard.is_pressed("q"):
     if keyboard.is_pressed("z"):
         keyboard.press_and_release("p")
-        while keyboard.is_pressed('z'):  # Z tuşuna basılı tutmayı engellemek için bekle
+        while keyboard.is_pressed('z'):  # Z tuşuna basılı tutmayı engellemek için 
             pass
         
     if keyboard.is_pressed("x"):
         keyboard.press_and_release("enter")
-        while keyboard.is_pressed('x'):  # X tuşuna basılı tutmayı engellemek için bekle
+        while keyboard.is_pressed('x'):  # X tuşuna basılı tutmayı engellemek için 
             pass
